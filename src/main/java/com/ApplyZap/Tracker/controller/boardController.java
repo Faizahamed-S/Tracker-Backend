ef4b3c3e56391cacee52348fc4426bba5a19396c
Faiz Ahamed Shaik <143540804+Faizahamed-S@users.noreply.github.com>
2025-09-29 22:05:08 -0700

    Replace every method with ResponseEntity

--- src/main/java/com/ApplyZap/Tracker/controller/boardController.java
+++ src/main/java/com/ApplyZap/Tracker/controller/boardController.java
@@ -1,13 +1,13 @@
 package com.ApplyZap.Tracker.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import com.ApplyZap.Tracker.service.boardService;
 import com.ApplyZap.Tracker.model.Application;
 import java.util.*;
+import org.springframework.http.ResponseEntity;
 
 
 @RestController
@@ -18,12 +18,18 @@ public class boardController {
     boardService boardService;
 
     @GetMapping("/applications")
-    public List<Application> getApplications(){
-        return boardService.getApplications();
+    public ResponseEntity<List<Application>> getApplications(){
+        return new ResponseEntity<>(boardService.getApplications(),HttpStatus.OK);
     }
 
     @GetMapping("/applications/{id}")
-    public Application getApplicationById(@PathVariable Long id){
-        return boardService.getApplicationById(id);
+    public ResponseEntity<Application> getApplicationById(@PathVariable Long id){
+        Application application = boardService.getApplicationById(id);
+        if(application == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        else{
+            return new ResponseEntity<>(application,HttpStatus.OK);
+        }
     }
 }
