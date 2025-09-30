b9a90ab4ee14e5e311c30d1f42e5eb8f2e3b79b8
Faiz Ahamed Shaik <143540804+Faizahamed-S@users.noreply.github.com>
2025-09-29 17:17:15 -0700

    Get Application & Get Application By Id
    - Get list of applications from H2
    - Get application by Id

--- src/main/java/com/ApplyZap/Tracker/controller/boardController.java
+++ src/main/java/com/ApplyZap/Tracker/controller/boardController.java
@@ -1,10 +1,29 @@
 package com.ApplyZap.Tracker.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.ApplyZap.Tracker.service.boardService;
+import com.ApplyZap.Tracker.model.Application;
+import java.util.*;
+
 
 @RestController
 @RequestMapping("/board")
 public class boardController {
 
+    @Autowired
+    boardService boardService;
+
+    @GetMapping("/applications")
+    public List<Application> getApplications(){
+        return boardService.getApplications();
+    }
+
+    @GetMapping("/applications/{id}")
+    public Application getApplicationById(@PathVariable Long id){
+        return boardService.getApplicationById(id);
+    }
 }
--- src/main/java/com/ApplyZap/Tracker/service/boardService.java
+++ src/main/java/com/ApplyZap/Tracker/service/boardService.java
@@ -1,7 +1,24 @@
 package com.ApplyZap.Tracker.service;
 
+import com.ApplyZap.Tracker.model.Application;
+import com.ApplyZap.Tracker.repository.boardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.*;
 
 @Service
 public class boardService {
+
+    @Autowired
+    boardRepository repo;
+
+    public List<Application> getApplications() {
+        return repo.findAll();
+    }
+
+    public Application getApplicationById(Long id) {
+        return repo.findById(id).orElse(new Application());
+    }
 }
