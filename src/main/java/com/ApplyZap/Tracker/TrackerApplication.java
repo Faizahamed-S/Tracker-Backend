272b76ebba89ab03736ede9a10199e28143b4ed2
Faiz Ahamed Shaik <143540804+Faizahamed-S@users.noreply.github.com>
2025-11-06 11:00:06 -0800

    Add Supabase connection variables

--- src/main/java/com/ApplyZap/Tracker/TrackerApplication.java
+++ src/main/java/com/ApplyZap/Tracker/TrackerApplication.java
@@ -1,13 +1,26 @@
 package com.ApplyZap.Tracker;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.*;
 
 @SpringBootApplication
 public class TrackerApplication {
+    static {
+        Dotenv dotenv = Dotenv.configure()
+                .directory(System.getProperty("user.dir"))
+                .ignoreIfMissing()
+                .load();
 
+        System.out.println("✅ Dotenv loaded entries:");
+        dotenv.entries().forEach(e -> System.out.println(e.getKey() + " = " + e.getValue()));
+
+        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(TrackerApplication.class, args);
+        System.out.println("From getenv: " + System.getenv("SUPABASE_PROJECT_URL"));
+        System.out.println("From getProperty: " + System.getProperty("SUPABASE_PROJECT_URL"));
 	}
 
 }
