d55e98074355e967bed459010f5361d23bcccac9
Faiz Ahamed Shaik <143540804+Faizahamed-S@users.noreply.github.com>
2026-02-16 12:32:40 -0800

    Clear for deployment

--- src/main/java/com/ApplyZap/Tracker/security/SecurityConfig.java
+++ src/main/java/com/ApplyZap/Tracker/security/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity(debug = false)
 @EnableMethodSecurity
 public class SecurityConfig {
 
